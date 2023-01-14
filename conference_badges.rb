dbe85bbc4fca91dba97f7b93b349c9f786890031
Demaris Ashmore <demiashmore@yahoo.com>
2023-01-13 20:49:43 -0700

    commit tree f5724e42ffb57ca0c0283293846f718e91cc52bf

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+    newArray = []
+    array.each_with_index{|name, index| 
+        newArray << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    }
+    newArray
+end
+
+def printer(array)
+    arr1 = batch_badge_creator(array)
+    arr1.map{|name| puts name}
+    arr2 = assign_rooms(array)
+    arr2.map{|assignment| puts assignment}
+end
